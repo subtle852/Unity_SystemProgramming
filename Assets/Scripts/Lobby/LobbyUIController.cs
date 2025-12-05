8814acc5b48bca26261d88b3df116699714af70e
subtle852 <subtle852@gmail.com>
2025-12-05 13:35:45 +0900

    feat: LobbyUIController ESC/BACK 처리 구현

--- Assets/Scripts/Lobby/LobbyUIController.cs
+++ Assets/Scripts/Lobby/LobbyUIController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,42 @@ public class LobbyUIController : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            AudioManager.Instance.PlaySFX(SFX.ui_button_click);
+
+            var frontUI = UIManager.Instance.GetCurrentFrontUI();
+            if (frontUI)
+            {
+                frontUI.CloseUI();
+            }
+            else
+            {
+                var uiData = new ConfirmUIData();
+                uiData.ConfirmType = ConfirmType.OK_CANCEL;
+                uiData.TitleTxt = "Quit";
+                uiData.DescTxt = "Do you want to quit game?";
+                uiData.OKBtnTxt = "Quit";
+                uiData.CancelBtnTxt = "Cancel";
+                uiData.OnClickOKBtn = () =>
+                {
+                    Application.Quit();
+                };
+                UIManager.Instance.OpenUI<ConfirmUI>(uiData);
+            }
+        }
+    }
+
     public void OnClickSettingsBtn()
     {
-        Logger.Log($"{GetType()}::OnClickTempBtn");
+        Logger.Log($"{GetType()}::OnClickSettingsBtn");
 
         var uiData = new BaseUIData();
         UIManager.Instance.OpenUI<SettingsUI>(uiData);
