909d6e533bfa132327b1df913397961fdf3d7596
subtle852 <subtle852@gmail.com>
2025-12-06 20:43:15 +0900

    feat: DataTableManager에 ItemData 추가

--- Assets/Scripts/Common/DataTableManager.cs
+++ Assets/Scripts/Common/DataTableManager.cs
@@ -8,16 +8,18 @@ public class DataTableManager : SingletonBehaviour<DataTableManager>
 {
     private const string DATA_PATH = "DataTable";
 
-    private const string CHAPTER_DATA_TABLE = "ChapterDataTable";
-    private List<ChapterData> ChapterDataTable = new List<ChapterData>();
 
     protected override void Init()
     {
         base.Init();
 
         LoadChapterDataTable();
+        LoadItemDataTable();
     }
 
+    #region CHAPTER_DATA
+    private const string CHAPTER_DATA_TABLE = "ChapterDataTable";
+    private List<ChapterData> ChapterDataTable = new List<ChapterData>();
     private void LoadChapterDataTable()
     {
         var parsedDataTable = CSVReader.Read($"{DATA_PATH}/{CHAPTER_DATA_TABLE}");
@@ -35,11 +37,39 @@ public class DataTableManager : SingletonBehaviour<DataTableManager>
             ChapterDataTable.Add(chapterData);
         }
     }
-
     public ChapterData GetChapterData(int chapterNo)
     {
         return ChapterDataTable.Where(item => item.ChapterNo == chapterNo).FirstOrDefault();
     }
+    #endregion
+
+    #region ITEM_DATA
+    private const string ITEM_DATA_TABLE = "ItemDataTable";
+    private List<ItemData> ItemDataTable = new List<ItemData>();
+
+    private void LoadItemDataTable()
+    {
+        var parsedDataTable = CSVReader.Read($"{DATA_PATH}/{ITEM_DATA_TABLE}");
+
+        foreach (var data in parsedDataTable)
+        {
+            var itemData = new ItemData
+            {
+                ItemId = Convert.ToInt32(data["item_id"]),
+                ItemName = data["item_name"].ToString(),
+                AttackPower = Convert.ToInt32(data["attack_power"]),
+                Defense = Convert.ToInt32(data["defense"]),
+            };
+
+            ItemDataTable.Add(itemData);
+        }
+    }
+
+    public ItemData GetItemData(int itemId)
+    {
+        return ItemDataTable.Where(item => item.ItemId == itemId).FirstOrDefault();
+    }
+    #endregion
 }
 
 public class ChapterData
@@ -48,4 +78,31 @@ public class ChapterData
     public int TotalStages;
     public int ChapterRewardGem;
     public int ChapterRewardGold;
+}
+
+public class ItemData
+{
+    public int ItemId;
+    public string ItemName;
+    public int AttackPower;
+    public int Defense;
+}
+
+public enum ItemType
+{
+    Weapon = 1,
+    Shield,
+    ChestArmor,
+    Gloves,
+    Boots,
+    Accessory
+}
+
+public enum ItemGrade
+{
+    Common = 1,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary,
 }
